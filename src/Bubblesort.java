c33e57eef6bb60e6a543781ac21c9513c08b40aa
Julian17072002 <55779857+Julian17072002@users.noreply.github.com>
2019-12-05 10:10:15 +0100

    Bubblesort

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -1,10 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Bubblesort {
 
+	static Random rn = new Random();
+	
 	public static void main(String[] args) {
+		System.out.println("Bubblesort");
+		System.out.println("==========");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Geben sie eine Natürliche Zahl für die Anzahl an Stellen in einem Array an: ");
+		int x = sc.nextInt();
+		int[] unsortiert = new int[x]; 
+		sc.close();
 		
-		int[] unsortiert={1,5,8,2,7,4};
+		for (int i = 0; i < unsortiert.length; i++) {
+			unsortiert[i] = rn.nextInt(); 
+	       System.out.println(unsortiert[i]);
+	    }	
+		System.out.println(" ");
+		System.out.println("Sortiert nach Bubblesort: ");
 		int[] sortiert=bubblesort(unsortiert);
+		System.out.println(" ");
 		
 		for (int i = 0; i<sortiert.length; i++) {
 			System.out.print(sortiert[i] + ", ");
@@ -13,6 +30,7 @@ public class Bubblesort {
 	}
 		public static int[] bubblesort(int[] zusortieren) {
 			int temp;
+			final long timeStart = System.currentTimeMillis();
 			for(int i=1; i<zusortieren.length; i++) {
 				for(int j=0; j<zusortieren.length-i; j++) {
 					if(zusortieren[j]>zusortieren[j+1]) {
@@ -23,6 +41,8 @@ public class Bubblesort {
 					
 				}
 			}
+			final long timeEnd = System.currentTimeMillis();
+			System.out.println("Verlaufszeit der Schleife: " + (timeEnd - timeStart) + " ms");
 			return zusortieren;
 		}
 }
